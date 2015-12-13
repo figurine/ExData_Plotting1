b2d20ad332d8aa46b88d65e3d1e7585a8160e74e
figurine <brand.new.colony@hotmail.com>
2015-12-13 21:36:00 +0000

    added plot 2, 3 and 4

---
+++ plot2.R
@@ -0,0 +1,12 @@
+source("getData.R", chdir = T)
+
+png(filename = "plot2.png",
+    width = 480,
+    height = 480)
+with(getData(), {
+  plot(Global_active_power ~ Datetime,
+       xlab = NA,
+       ylab = "Global Active Power (kilowatts)",
+       type = "l")
+})
+dev.off()
